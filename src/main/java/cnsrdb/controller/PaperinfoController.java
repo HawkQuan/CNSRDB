0ba9511e85facba61c3a41cf44c2548c93dcffef
Xie <934460848@qq.con>
2018-01-03 19:30:01 +0800

    数据库

--- src/main/java/cnsrdb/controller/PaperinfoController.java
+++ src/main/java/cnsrdb/controller/PaperinfoController.java
@@ -10,13 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 public class PaperinfoController {
     @Autowired
     private PaperinfoService paperinfoService;
 
+    List<Paperinfo>paperinfos = new LinkedList<Paperinfo>();
+
+    /**
+     * 按照学号查找专利信息,POST
+     * @param tid 学号
+     * @param model
+     * @return  专利那一页
+     */
     @RequestMapping(value = "/getPaper",method = RequestMethod.POST)
-    public String getPaper(@RequestParam(value = "tid",required = false) String tid, Model model){
+    public String getPaperinfo(@RequestParam(value = "tid",required = false) String tid, Model model){
+
+        //-----------------调试信息------------------
+        System.out.println("按学号查看专利时：");
+        System.out.println(tid);
+        //-------------------------------------
 
         Paperinfo paperinfo = null;
         paperinfo = paperinfoService.getPaperinfo(tid);//带你用service层里的函数
@@ -24,4 +40,73 @@ public class PaperinfoController {
         return Constant.Paper;//返回到paper界面
     }
 
+    /**
+     * GET方式获得所有专利信息
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/getPapers",method = RequestMethod.GET)
+    public String getPaperinfos(Model model){
+
+        paperinfos = paperinfoService.getPaperinfos();
+        model.addAttribute("papers",paperinfos);
+        return Constant.Paper;
+    }
+
+    /**
+     * 删除专利，POST
+     * @param grade
+     * @param tid
+     * @param title
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "deletepaper",method = RequestMethod.POST)
+    public String deletePaperinfo(@RequestParam(value = "grade",required = false)String grade,
+                                  @RequestParam(value = "tid",required = false) Long tid,
+                                  @RequestParam(value = "title",required = false)String title,
+                                  Model model){
+        //-----------------调试信息------------------
+        System.out.println("删除专利时：");
+        System.out.println(grade);
+        System.out.println(tid);
+        System.out.println(title);
+        //-------------------------------------
+
+        Paperinfo paperinfo = new Paperinfo(grade,tid,title);
+        paperinfoService.deletePaperinfo(paperinfo);
+
+
+        paperinfos = paperinfoService.getPaperinfos();
+        model.addAttribute("papers",paperinfos);
+        return Constant.Paper;
+    }
+
+    /**
+     * 添加专利信息，POST
+     * @param grade
+     * @param tid
+     * @param title
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/addpaper",method = RequestMethod.POST)
+    public String addPaperinfo(@RequestParam(value = "grade",required = false)String grade,
+                               @RequestParam(value = "tid",required = false) Long tid,
+                               @RequestParam(value = "title",required = false)String title,
+                               Model model){
+        //-----------------调试信息------------------
+        System.out.println("添加专利时：");
+        System.out.println(grade);
+        System.out.println(tid);
+        System.out.println(title);
+        //-------------------------------------
+
+        Paperinfo paperinfo = new Paperinfo(grade,tid,title);
+        paperinfoService.addPaperinfo(paperinfo);
+
+        paperinfos = paperinfoService.getPaperinfos();
+        model.addAttribute("papers",paperinfos);
+        return Constant.Paper;
+    }
 }
